5fdc143c51305cd05de8e890eb9d2486b28226b1
MasoumeMohebbi <88876100+MasoumeMohebbi@users.noreply.github.com>
2023-04-03 21:56:08 +0330

    tailwind config file added

---
+++ postcss/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"PT Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
